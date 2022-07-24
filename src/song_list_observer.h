ce56dfd15f919bd3ae5e2aa06cdc0de31ab51a65
Duy Le <94324394+SabreBirdOne@users.noreply.github.com>
2022-07-23 21:18:58 -0400

    Documentation for SongListObserver

--- src/song_list_observer.h
+++ src/song_list_observer.h
@@ -4,10 +4,20 @@
 #include <list>
 #include "song_list_entry.h"
 
+/*
+	A concrete class. Observes a SongList, which notifies
+	the observer when SongList changes.
+
+	(7/23) This class is currently a poltergeist. >:(
+*/
 class SongListObserver {
 private:
 	std::list<SongListEntry> current_data;
 public:
+	/* SPECS:
+		@PARAM: data: the data from the SongList object
+		@EFFECT: updates the Observer's data to match the SongList.
+	*/
 	void update(const std::list<SongListEntry>& data);
 };
 
